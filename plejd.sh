5b0226e8fd544825e8652e86ca4ed54b78f006d8
Alexader <alexader.herlin@outlook.com>
2022-02-19 20:40:05 +0100

    Update plejd extraction script

--- plejd.sh
+++ plejd.sh
@@ -1,22 +1,28 @@
 #/bin/bash
 
-# DON'T RUN.
-# This is very much WIP. I've just stored what i've used to extract my token.
+UNAME=$(uname)
+if [ $UNAME != 'Darwin' ]; then 
+    echo "For now only works on mac"
+    exit 0
+fi
 
-# install: sudo i -g ibackuptool https://github.com/richinfante/iphonebackuptools
-# System preferences -> Security & Privacy -> Privacy -> tick Full disk access for terminal
+PREFIX=$(npm config get prefix)
+NODE_IBACKUP_DIR=$PREFIX/lib/node_modules/ibackuptool
 
-# # To make it run on mac we need to fix the line breaks in the files.
-# sudo find /usr/local/lib/node_modules/ibackuptool/**/*js -exec sudo dos2unix {} \;
+if [ ! -d "$NODE_IBACKUP_DIR" ]; then
+then
+    sudo npm i -g ibackuptool
+    echo "Installing ibackuptool https://github.com/richinfante/iphonebackuptools" 
+    # To make it run on mac we need to fix the line breaks in the files.
+    sudo find $NODE_IBACKUP_DIR/**/*js -exec sudo dos2unix {} \;
+    echo "System preferences -> Security & Privacy -> Privacy -> tick Full disk access for terminal"
+fi
 
-raw=$(ibackuptool -l -f json)
-UUID=$(echo ${raw} | sed 's/run backups.list //' | jq '.[0].udid' | sed 's/"//g')
+RAW=$(ibackuptool -l -f json)
+UUID=$(echo ${RAW} | sed 's/run backups.list //' | jq '.[0].udid' | sed 's/"//g')
 mkdir temp
-q=$(sudo ibackuptool -b ${UUID} -r backup.files --extract ./temp --regex-filter '\.(site)$')
+Q=$(sudo ibackuptool -b ${UUID} -r backup.files --extract ./temp --regex-filter '\.(site)$')
 mv temp/App/com.plejd.consumer.light/Documents/**/*.site ./temp/site.json
 cat ./temp/site.json | jq '.PlejdMesh.CryptoKey' | sed 's/-//g' | sed 's/"//g'
 cat ./temp/site.json | jq '.PlejdMesh._outputAddresses' | grep -v '\$type' | jq '.[][]' 
 rm -rf temp
-
-
-
